2780a864b2c95b1d4ded885bb02cbe528d2b0cbb
TanPhat310309 <duckydaily044@gmail.com>
2026-03-23 10:30:56 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ function App() {
   return (
     <div className='container'>
       <h1 className="main-title">Danh sách sinh viên</h1>
-      <div className="title">
-        <Student name="Lý Tấn Phát" age="17" className="26.SP.IT3" hobby="Anime/Manga/LightNovel"/>
+      <div className="title1">
+        <Student name="Lý Tấn Phát" age="17" className="26.SP.IT3" hobby="Anime/Manga/Manhwa/Manhua"/>
       </div>
 
-      <div className="title">
+      <div className="title2">
         <Student name="Nguyễn Thanh Nhật" age="17" className="26.SP.IT3" hobby="Nấu ăn"/>
       </div>
 
-      <div className="title">
+      <div className="title3">
         <Student name="Nguyễn Hoàng Cát Tường" age="17" className="26.SP.IT3" hobby="Ngủ nướng"/>
       </div>
 
+      <div className="title4">
+        <Student name="Ngô Minh Tân" age="17" className="26.SP.IT3" hobby="Chơi game"/>
+      </div>
     </div>
   );
 }
